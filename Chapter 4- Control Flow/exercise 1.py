d27f67d7ac9d7d713161fb19dccc277922825db4
MikeSamaaa <111354382+MikeSamaaa@users.noreply.github.com>
2022-11-16 17:25:26 +0400

    Delete exercise 1.py

--- Chapter 4- Control Flow/exercise 1.py
+++
@@ -1,7 +0,0 @@
-alien_color = "Red"
-if alien_color == "Red":
-      print("The player just earned 5 points.")
-
-alien_color = "Green"
-if alien_color == "Red":
-      print("The player just earned 5 points")

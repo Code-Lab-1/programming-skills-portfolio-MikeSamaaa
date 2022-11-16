88dfb42a60db72aa28966f2b1127c65ef90e0ae8
MikeSamaaa <111354382+MikeSamaaa@users.noreply.github.com>
2022-11-16 17:18:51 +0400

    Chapter 4 answers

---
+++ Chapter 4- Control Flow/exercise 1.py
@@ -0,0 +1,7 @@
+alien_color = "Red"
+if alien_color == "Red":
+      print("The player just earned 5 points.")
+
+alien_color = "Green"
+if alien_color == "Red":
+      print("The player just earned 5 points")
---
+++ Chapter 4- Control Flow/exercise 2.py
@@ -0,0 +1,5 @@
+alien_color = "Green"
+if alien_color == "Green":
+      print("The player just earned 5 points for shooting the alien.")
+else:
+      print("The player just earned 10 points")
